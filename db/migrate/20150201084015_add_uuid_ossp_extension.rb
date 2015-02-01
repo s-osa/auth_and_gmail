3fd80c47655e771eca8e004c4ef0b8307047e722
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-01 17:49:22 +0900

    Install postgresql extension 'uuid-ossp'.

---
+++ db/migrate/20150201084015_add_uuid_ossp_extension.rb
@@ -0,0 +1,13 @@
+class AddUuidOsspExtension < ActiveRecord::Migration
+  def up
+    execute <<-SQL.strip_heredoc
+      CREATE EXTENSION "uuid-ossp";
+    SQL
+  end
+
+  def down
+    execute <<-SQL.strip_heredoc
+      DROP EXTENSION "uuid-ossp";
+    SQL
+  end
+end
