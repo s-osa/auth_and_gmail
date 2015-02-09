32a00b35b33eebbbff462567e53e2cf9795cde41
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-09 16:27:00 +0900

    Allow GET logout.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get    "login"  => "sessions#new"
   get    "oauth2" => "sessions#oauth2"
   delete "logout" => "sessions#destroy"
+  get    "logout" => "sessions#destroy"
 
   root "users#index"
 end
