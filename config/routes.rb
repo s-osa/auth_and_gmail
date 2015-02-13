7e2d74405084d93333fab86de64a8dce42786669
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-13 17:58:57 +0900

    Hide other user's info.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get    "login"  => "sessions#new"
   get    "oauth2" => "sessions#oauth2"
   delete "logout" => "sessions#destroy"
-  get    "logout" => "sessions#destroy"
 
   post "draft" => "drafts#create"
 
